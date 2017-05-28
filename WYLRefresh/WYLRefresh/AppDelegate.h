be18bdd71601ed501ed3f3301febb757156befd3
wangyanlong <553836854@qq.com>
2017-05-28 22:47:53 +0800

    创建基本工程

---
+++ WYLRefresh/WYLRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WYLRefresh
+//
+//  Created by wyl on 2017/5/28.
+//  Copyright © 2017年 wyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
